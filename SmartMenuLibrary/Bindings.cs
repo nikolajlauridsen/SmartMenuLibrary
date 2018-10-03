d6a5a80d829426ce925446e96dfa00bb97898411
Molex <simplesleeptimer@gmail.com>
2018-10-03 12:10:19 +0200

    Add Bindings

---
+++ SmartMenuLibrary/Bindings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartMenuLibrary {
+    class Bindings {
+        Dictionary<string, Action> binds;
+
+        public Bindings() {
+            binds = new Dictionary<string, Action>;
+        }
+
+        public void Bind(String id, Action action) {
+            binds[id] = action;
+        }
+
+        public void Call(String id) {
+            Action action = binds[id];
+            action();
+        }
+    }
+}
